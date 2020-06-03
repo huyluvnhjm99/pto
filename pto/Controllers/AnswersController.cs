62511bd9bf5fa4f0cb1092e11f7a780dbf7a28f7
huyluvnhjm99 <34624305+huyluvnhjm99@users.noreply.github.com>
2020-06-03 08:24:25 +0700

    2

---
+++ pto/Controllers/AnswersController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using pto.Models;
+
+namespace pto.Controllers
+{
+    public class AnswersController : ApiController
+    {
+        private ptoEntities db = new ptoEntities();
+
+        // GET: api/answers
+        public IQueryable<answer> Getanswers()
+        {
+            return db.answers;
+        }
+
+        // GET: api/answers/5
+        [ResponseType(typeof(answer))]
+        public IHttpActionResult Getanswer(int id)
+        {
+            answer answer = db.answers.Find(id);
+            if (answer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(answer);
+        }
+
+        // PUT: api/answers/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putanswer(int id, answer answer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != answer.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(answer).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!answerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/answers
+        [ResponseType(typeof(answer))]
+        public IHttpActionResult Postanswer(answer answer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.answers.Add(answer);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = answer.id }, answer);
+        }
+
+        // DELETE: api/answers/5
+        [ResponseType(typeof(answer))]
+        public IHttpActionResult Deleteanswer(int id)
+        {
+            answer answer = db.answers.Find(id);
+            if (answer == null)
+            {
+                return NotFound();
+            }
+
+            db.answers.Remove(answer);
+            db.SaveChanges();
+
+            return Ok(answer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool answerExists(int id)
+        {
+            return db.answers.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ pto/Controllers/PersonalityTestController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using pto.Models;
+
+namespace pto.Controllers
+{
+    public class PersonalityTestController : ApiController
+    {
+        private ptoEntities db = new ptoEntities();
+
+        // GET: api/PersonalityTest
+        public IQueryable<personality_test> Getpersonality_test()
+        {
+            return db.personality_test;
+        }
+
+        // GET: api/PersonalityTest/5
+        [ResponseType(typeof(personality_test))]
+        public IHttpActionResult Getpersonality_test(int id)
+        {
+            personality_test personality_test = db.personality_test.Find(id);
+            if (personality_test == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(personality_test);
+        }
+
+        // PUT: api/PersonalityTest/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putpersonality_test(int id, personality_test personality_test)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != personality_test.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(personality_test).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!personality_testExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/PersonalityTest
+        [ResponseType(typeof(personality_test))]
+        public IHttpActionResult Postpersonality_test(personality_test personality_test)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.personality_test.Add(personality_test);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = personality_test.id }, personality_test);
+        }
+
+        // DELETE: api/PersonalityTest/5
+        [ResponseType(typeof(personality_test))]
+        public IHttpActionResult Deletepersonality_test(int id)
+        {
+            personality_test personality_test = db.personality_test.Find(id);
+            if (personality_test == null)
+            {
+                return NotFound();
+            }
+
+            db.personality_test.Remove(personality_test);
+            db.SaveChanges();
+
+            return Ok(personality_test);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool personality_testExists(int id)
+        {
+            return db.personality_test.Count(e => e.id == id) > 0;
+        }
+    }
+}
